0f91788745a2679806abd6c8680e4525ad60e26c
Mohit kumar <mohitsinghoi501@gmail.com>
2025-09-24 22:16:07 +0530

    mid

--- 10todocontextLoacl/src/Components/Footer.jsx
+++ 10todocontextLoacl/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="text-center text-gray-300 mt-6 py-4 border-t border-gray-600 mt-[200px]">
+    <footer className="text-center text-gray-300 py-4 border-t border-gray-600 mt-[200px]">
       <p className="text-sm">© {new Date().getFullYear()} Todo App. Built with ❤️ using React.</p>
     </footer>
   );
